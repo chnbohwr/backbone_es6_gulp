75e6d5f8607d74259bc40aa1c658f4bb74bf1f80
Hyman_Chen <hyman_chen@wistron.com>
2018-03-06 16:23:18 +0800

    add config

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,35 @@ const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 const inject = require('gulp-inject');
 
-const distFolder = 'dist';
+const distFolder = 'dist/';
+const sourceFolder = 'src/';
 
 gulp.task('lib', () => gulp
-  .src('src/lib/*.js')
-  .pipe(gulp.dest(`${distFolder}/lib`))
+  .src('src/lib/*.js', { base: sourceFolder })
+  .pipe(gulp.dest(distFolder))
 );
 
 gulp.task('js', () => gulp
-  .src('src/js/*.js')
+  .src('src/js/*.js', { base: sourceFolder })
   .pipe(babel({
     presets: ['env']
   }))
   .pipe(uglify())
-  .pipe(gulp.dest(`${distFolder}/js`))
+  .pipe(gulp.dest(distFolder))
   .pipe(browserSync.stream())
 );
 
 gulp.task('html', () => gulp
-  .src('src/*.html')
+  .src('src/*.html', { base: sourceFolder })
   .pipe(inject(gulp.src(['src/lib/*.js', 'src/js/*.js'], { read: false }), { relative: true }))
-  .pipe(gulp.dest('./dist'))
+  .pipe(gulp.dest(distFolder))
   .pipe(browserSync.stream())
 );
 
 // Watch Sass & Serve
 gulp.task('server', ['default'], () => {
   browserSync.init({
-    server: `./${distFolder}`
+    server: distFolder
   });
   gulp.watch('src/js/*.js', ['js']);
   gulp.watch('src/*.html', ['html']);
